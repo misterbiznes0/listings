f4ff86aa56a139c277b0662aba2b93cfa75cc43d
misterbiznes0 <nnaumov2394@gmail.com>
2026-06-25 22:31:15 +0300

    Create list8.py

---
+++ list8.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('/profile/')
+
+        messages.error(request, 'Неверный логин или пароль.')
+
+    return render(request, 'login.html')
